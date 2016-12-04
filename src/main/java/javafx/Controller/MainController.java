a5c82af1d97fe2841b2ec9fa1e031aed4dc79858
Denis Misuyra <script972@gmail.com>
2016-12-04 13:40:10 +0200

    maven, fx

---
+++ src/main/java/javafx/Controller/MainController.java
@@ -0,0 +1,7 @@
+package javafx.Controller;
+
+/**
+ * Created by script972 on 04.12.2016.
+ */
+public class MainController {
+}
---
+++ src/main/java/javafx/Main.java
@@ -0,0 +1,26 @@
+package javafx;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ * Created by script972 on 04.12.2016.
+ */
+public class Main extends Application {
+    public static void main(String[] args) throws Exception {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        String fxmlFile = "/fxml/MainWindow.fxml";
+        FXMLLoader loader = new FXMLLoader();
+        Parent root = (Parent) loader.load(getClass().getResourceAsStream(fxmlFile));
+        stage.setTitle("JavaFX and Maven");
+        stage.setScene(new Scene(root));
+        stage.show();
+    }
+}
